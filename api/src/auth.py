00888724e834e91963a642fdbf88bfd794970e40
Fabian <fabian@fabianhaupt.de>
2018-11-29 07:47:24 -0500

    backend also working

--- api/src/auth.py
+++ api/src/auth.py
@@ -5,7 +5,7 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'https://yape-web.auth0.com/'
+AUTH0_DOMAIN = 'yape-web.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'https://yape.iscinternal.com:5000'
 
